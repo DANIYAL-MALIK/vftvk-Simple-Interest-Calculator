fafd6c03f2f11ee1d781e8c2b2cb395c7ecd2365
Daniyal Malik <myEmail@example.com>
2022-01-11 17:12:15 +0500

    ADD the complete code

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  let amount = document.getElementById("principal").value;
+  let Ir = document.getElementById("rate").value;
+  ye = new Date();
+  y = document.getElementById("years").value;
+  year=((ye.getYear()-100))+y
+  document.write(
+    "If you deposit ",
+    amount,
+    " at an intrest rate of ",
+    Ir,
+    " you will receive an amount of ",
+    (Ir * amount * y) / 100,
+    " in the year ",
+    y
+  );
 }
-        
