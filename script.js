1b9904ce3171474f12db51545a25dfd3b89e23ac
Daniyal Malik <myEmail@example.com>
2022-01-11 21:31:04 +0500

    Again modified

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function compute() {
-  let amount = document.getElementById("principal").value;
-  let Ir = document.getElementById("rate").value;
-  ye = new Date();
-  y = document.getElementById("years").value;
-  year=((ye.getYear()-100))+y
-  document.write(
-    "If you deposit ",
-    amount,
-    " at an intrest rate of ",
-    Ir,
-    " you will receive an amount of ",
-    (Ir * amount * y) / 100,
-    " in the year ",
-    y
-  );
+  let Amount = document.getElementById("principal").value;
+  let Rate=document.getElementById("rate").value;
+  if (Amount==""){
+    alert("please enter the amount");
+    Amount.focus();
+    return false;
+  }
+  else if(Rate==""){
+    alert("please enter the rate");
+    Rate.focus();
+    return false;
+  }
+  else{
+    alert(Rate*Amount);
+  }
 }
